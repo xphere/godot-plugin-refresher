f4b8d797ea03cef234656959bc14ac12bc2138c1
Berny Cantos <be@rny.cc>
2022-10-27 22:14:17 +0200

    Add support for Godot 4.* series

--- addons/godot-plugin-refresher/plugin_refresher.gd
+++ addons/godot-plugin-refresher/plugin_refresher.gd
@@ -1,53 +1,60 @@
-tool
+@tool
 extends HBoxContainer
 
 signal request_refresh_plugin(p_name)
 signal confirm_refresh_plugin(p_name)
 
-onready var options = $OptionButton
+@onready var options = $OptionButton
 
-func _ready():
+var icon: Texture2D
+
+
+func _ready() -> void:
 	if get_tree().edited_scene_root == self:
-		return # This is the scene opened in the editor!
-	$RefreshButton.icon = get_icon("Reload", "EditorIcons")
+		return
+	# This is the scene opened in the editor!
+	$RefreshButton.icon = icon
 
 
-func update_items(p_plugins):
+func update_items(p_plugins: Dictionary) -> void:
 	if not options:
 		return
+	
 	options.clear()
-	var plugin_dirs = p_plugins.keys()
+	var plugin_dirs : Array = p_plugins.keys()
 	for idx in plugin_dirs.size():
-		var plugin_dirname = plugin_dirs[idx]
-		var plugin_name = p_plugins[plugin_dirname]
+		var plugin_dirname : String = plugin_dirs[idx]
+		var plugin_name : String = p_plugins[plugin_dirname]
 		options.add_item(plugin_name, idx)
 		options.set_item_metadata(idx, plugin_dirname)
 
 
-func select_plugin(p_name):
+func select_plugin(p_name: String) -> void:
 	if not options:
 		return
-	if p_name == null or p_name.empty():
+	
+	if p_name.is_empty():
 		return
-
+	
 	for idx in options.get_item_count():
-		var plugin = options.get_item_metadata(idx)
+		var plugin : String = options.get_item_metadata(idx)
 		if plugin == p_name:
 			options.selected = options.get_item_id(idx)
 			break
 
 
-func _on_RefreshButton_pressed():
+func _on_RefreshButton_pressed() -> void:
 	if options.selected == -1:
 		return # nothing selected
 
-	var plugin = options.get_item_metadata(options.selected)
-	if not plugin or plugin.empty():
+	var plugin : String = options.get_item_metadata(options.selected)
+	if plugin.is_empty():
 		return
-	emit_signal("request_refresh_plugin", plugin)
+	
+	request_refresh_plugin.emit(plugin)
 
 
-func show_warning(p_name):
+func show_warning(p_name: String) -> void:
 	$ConfirmationDialog.dialog_text = """
 		Plugin `%s` is currently disabled.\n
 		Do you want to enable it now?
@@ -55,6 +62,6 @@ func show_warning(p_name):
 	$ConfirmationDialog.popup_centered()
 
 
-func _on_ConfirmationDialog_confirmed():
-	var plugin = options.get_item_metadata(options.selected)
-	emit_signal("confirm_refresh_plugin", plugin)
+func _on_ConfirmationDialog_confirmed() -> void:
+	var plugin : String = options.get_item_metadata(options.selected)
+	confirm_refresh_plugin.emit(plugin)
--- addons/godot-plugin-refresher/plugin_refresher_plugin.gd
+++ addons/godot-plugin-refresher/plugin_refresher_plugin.gd
@@ -1,53 +1,55 @@
-tool
+@tool
 extends EditorPlugin
 
-const ADDONS_PATH = "res://addons/"
-const PLUGIN_CONFIG_DIR = "plugins/plugin_refresher"
-const PLUGIN_CONFIG = "settings.cfg"
-const SETTINGS = "settings"
-const SETTING_RECENT = "recently_used"
+const ADDONS_PATH := "res://addons/"
+const PLUGIN_CONFIG_DIR := "plugins/plugin_refresher"
+const PLUGIN_CONFIG := "settings.cfg"
+const SETTINGS := "settings"
+const SETTING_RECENT := "recently_used"
 
-var plugin_config = ConfigFile.new()
-var refresher
+var plugin_config := ConfigFile.new()
+var refresher: Node
 
-func _enter_tree():
-	refresher = preload("plugin_refresher.tscn").instance()
+
+func _enter_tree() -> void:
+	refresher = preload("plugin_refresher.tscn").instantiate()
+	refresher.icon = get_editor_interface().get_base_control().get_theme_icon("Reload", "EditorIcons")
 	add_control_to_container(CONTAINER_TOOLBAR, refresher)
-
+	
 	# Watch whether any plugin is changed, added or removed on the filesystem
-	var efs = get_editor_interface().get_resource_filesystem()
-	efs.connect("filesystem_changed", self, "_on_filesystem_changed")
-
-	refresher.connect("request_refresh_plugin", self, "_on_request_refresh_plugin")
-	refresher.connect("confirm_refresh_plugin", self, "_on_confirm_refresh_plugin")
-
+	var efs := get_editor_interface().get_resource_filesystem()
+	efs.filesystem_changed.connect(_on_filesystem_changed)
+	
+	refresher.request_refresh_plugin.connect(_on_request_refresh_plugin)
+	refresher.confirm_refresh_plugin.connect(_on_confirm_refresh_plugin)
+	
 	_reload_plugins_list()
 	_load_settings()
 
 
-func _exit_tree():
+func _exit_tree() -> void:
 	remove_control_from_container(CONTAINER_TOOLBAR, refresher)
-	refresher.free()
+	refresher.queue_free()
 
 
-func _reload_plugins_list():
-	var refresher_dir = get_plugin_path().get_file()
-	var plugins = {}
-	var origins = {}
+func _reload_plugins_list() -> void:
+	var refresher_dir := get_plugin_path().get_file()
+	var plugins := {}
+	var origins := {}
 
-	var dir = Directory.new()
-	dir.open(ADDONS_PATH)
-	dir.list_dir_begin(true, true)
-	var file = dir.get_next()
+	var dir := DirAccess.open(ADDONS_PATH)
+	dir.list_dir_begin()
+	var file := dir.get_next()
 	while file:
-		var addon_dir = ADDONS_PATH.plus_file(file)
+		var addon_dir := ADDONS_PATH.path_join(file)
 		if dir.dir_exists(addon_dir) and file != refresher_dir:
-			var display_name = file
-			var plugin_config_path = addon_dir.plus_file("plugin.cfg")
+			var display_name := file
+			var plugin_config_path := addon_dir.path_join("plugin.cfg")
 			if not dir.file_exists(plugin_config_path):
 				file = dir.get_next()
 				continue # not a plugin
-			var plugin_cfg = ConfigFile.new()
+			
+			var plugin_cfg := ConfigFile.new()
 			plugin_cfg.load(plugin_config_path)
 			display_name = plugin_cfg.get_value("plugin", "name", file)
 			if not display_name in origins:
@@ -55,83 +57,83 @@ func _reload_plugins_list():
 			else:
 				origins[display_name].append(file)
 			plugins[file] = display_name
+		
 		file = dir.get_next()
-
+	
 	# Specify the exact plugin name in parenthesis in case of naming collisions.
 	for display_name in origins:
-		var plugin_names = origins[display_name]
+		var plugin_names : Array = origins[display_name]
 		if plugin_names.size() > 1:
 			for n in plugin_names:
 				plugins[n] = "%s (%s)" % [display_name, n]
-
+	
 	refresher.update_items(plugins)
 
 
-func _load_settings():
-	var path = get_config_path()
+func _load_settings() -> void:
+	var path := get_config_path()
 
-	var fs = Directory.new()
-	if not fs.file_exists(path):
+	if not FileAccess.file_exists(path):
 		# Create new if running for the first time
-		var config = ConfigFile.new()
-		fs.make_dir_recursive(path.get_base_dir())
+		var config := ConfigFile.new()
+		DirAccess.make_dir_recursive_absolute(path.get_base_dir())
 		config.save(path)
 	else:
 		plugin_config.load(path)
 
 
-func _save_settings():
+func _save_settings() -> void:
 	plugin_config.save(get_config_path())
 
 
-func get_config_path():
-	var dir = get_editor_interface().get_editor_settings().get_project_settings_dir()
-	var home = dir.plus_file(PLUGIN_CONFIG_DIR)
-	var path = home.plus_file(PLUGIN_CONFIG)
-
+func get_config_path() -> String:
+	var dir := get_editor_interface().get_editor_paths().get_project_settings_dir()
+	var home := dir.path_join(PLUGIN_CONFIG_DIR)
+	var path := home.path_join(PLUGIN_CONFIG)
+	
 	return path
 
 
-func _on_filesystem_changed():
+func _on_filesystem_changed() -> void:
 	if refresher:
 		_reload_plugins_list()
 		refresher.select_plugin(get_recent_plugin())
 
 
-func get_recent_plugin():
+func get_recent_plugin() -> String:
 	if not plugin_config.has_section_key(SETTINGS, SETTING_RECENT):
-		return null # not saved yet
-
+		return "" # not saved yet
+	
 	var recent = plugin_config.get_value(SETTINGS, SETTING_RECENT)
 	return recent
 
 
-func _on_request_refresh_plugin(p_name):
-	assert(not p_name.empty())
-
-	var disabled = not get_editor_interface().is_plugin_enabled(p_name)
-	if disabled:
-		refresher.show_warning(p_name)
-	else:
+func _on_request_refresh_plugin(p_name: String) -> void:
+	assert(not p_name.is_empty())
+	
+	var enabled := get_editor_interface().is_plugin_enabled(p_name)
+	if enabled:
 		refresh_plugin(p_name)
+	else:
+		refresher.show_warning(p_name)
 
 
-func _on_confirm_refresh_plugin(p_name):
+func _on_confirm_refresh_plugin(p_name: String) -> void:
 	refresh_plugin(p_name)
 
 
-func get_plugin_path():
+func get_plugin_path() -> String:
 	return get_script().resource_path.get_base_dir()
 
 
-func refresh_plugin(p_name):
+func refresh_plugin(p_name) -> void:
 	print("Refreshing plugin: ", p_name)
-
-	var enabled = get_editor_interface().is_plugin_enabled(p_name)
+	
+	var enabled := get_editor_interface().is_plugin_enabled(p_name)
 	if enabled: # can only disable an active plugin
 		get_editor_interface().set_plugin_enabled(p_name, false)
-
+	
 	get_editor_interface().set_plugin_enabled(p_name, true)
-
+	
 	plugin_config.set_value(SETTINGS, SETTING_RECENT, p_name)
 	_save_settings()
